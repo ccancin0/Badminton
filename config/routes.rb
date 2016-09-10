1a7ba1dd7f91b945b78dbfd6e77dfd04b99fe243
ccancin0 <ccancino@memphis.edu>
2016-09-10 14:03:27 -0500

    config/routes.rb
    correction 2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/sdflming'
-
   get 'sdflming', to: 'static_pages#sdflming', as: 'sdflming'
 
+  # Set the default page for the web app.
   root to: 'static_pages#sdflming'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
