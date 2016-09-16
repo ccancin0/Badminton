a69f4530d779f4177b497335a9f6d236aef0cb50
Carlos <ccanin0>
2016-09-16 20:14:15 +0000

    route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-<<<<<<< HEAD
-=======
-Rails.application.routes.draw do
-
-  get 'acklatt', to: 'static_pages#acklatt', as: 'acklatt'
-  get 'sdflming', to: 'static_pages#sdflming', as: 'sdflming'
-
-  # Set the default page for the web app.
-  root to: 'static_pages#sdflming'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
->>>>>>> fd73ce9f5645083e898df23f29eb58820cd5de73
-=======
->>>>>>> f4ef493df3c7d45ef2e4e1c418e9162e05020095
